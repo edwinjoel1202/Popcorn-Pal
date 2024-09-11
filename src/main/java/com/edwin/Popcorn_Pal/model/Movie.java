f21fefda7dec33c32cb1ce649de02ae416babbf8
edwinjoel1202 <edwinjoel1204@gmail.com>
2024-09-11 19:13:46 +0530

    Movie entity model class created

---
+++ src/main/java/com/edwin/Popcorn_Pal/model/Movie.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.edwin.Popcorn_Pal.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+
+
+/**
+ *
+ * @author edwin
+ */
+
+@Entity
+@Table(name = "Movie")
+@Getter
+@Setter
+public class Movie {
+    
+    @Id
+    private int movieId;
+    
+    private String title;
+    
+    private String releaseDate;
+    
+    private String posterUrl;
+    
+    private String description; 
+}

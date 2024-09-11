17d55084ef106a7e1d58ce3ac470d6b985833b96
edwinjoel1202 <edwinjoel1204@gmail.com>
2024-09-11 18:45:51 +0530

    Spring project initialized

---
+++ src/main/java/com/edwin/Popcorn_Pal/PopcornPalApplication.java
@@ -0,0 +1,13 @@
+package com.edwin.Popcorn_Pal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopcornPalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopcornPalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edwin/Popcorn_Pal/PopcornPalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edwin.Popcorn_Pal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopcornPalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

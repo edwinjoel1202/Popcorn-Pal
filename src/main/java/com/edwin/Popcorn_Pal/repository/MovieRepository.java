19a3835ae60ef40f5bcb87744af4c998b2cd3155
edwinjoel1202 <edwinjoel1204@gmail.com>
2024-09-12 18:25:49 +0530

    MovieRepository Interface repo layer added

---
+++ src/main/java/com/edwin/Popcorn_Pal/repository/MovieRepository.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.edwin.Popcorn_Pal.repository;
+
+import com.edwin.Popcorn_Pal.model.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author edwin
+ */
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, Integer>{
+    
+}

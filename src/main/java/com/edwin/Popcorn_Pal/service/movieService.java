51f63ce7870a83ce6bcd48e4377679da20c391c0
edwinjoel1202 <edwinjoel1204@gmail.com>
2024-09-16 10:38:38 +0530

    Movie service layer modified based on Movie.java

--- src/main/java/com/edwin/Popcorn_Pal/service/movieService.java
+++ src/main/java/com/edwin/Popcorn_Pal/service/movieService.java
@@ -1,46 +1,61 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.edwin.Popcorn_Pal.service;
 
 import com.edwin.Popcorn_Pal.model.Movie;
 import com.edwin.Popcorn_Pal.repository.MovieRepository;
+import jakarta.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Service;
 
 /**
+ * Service class for managing movies.
  *
  * @author edwin
  */
-
 @Service
+@Transactional
 public class movieService {
-    
+
     @Autowired
-    private MovieRepository movieRepo;
-    
-    // Method to get all the movies from the Movie Entity..:)
+    private MovieRepository movieRepository;
+
+    /**
+     * Fetches all movies from the database.
+     *
+     * @return List of movies.
+     */
     public List<Movie> getAllMovies() {
-        return movieRepo.findAll();
+        return movieRepository.findAll();
     }
-    
-    //Method to get a specific movie with its id..:)
-    public Movie getMovieById(Integer id) {
-        return movieRepo.findById(id).orElse(null);
+
+    /**
+     * Fetches a movie by its ID.
+     *
+     * @param movieId The UUID of the movie to find.
+     * @return An Optional containing the movie if found, or empty if not found.
+     */
+    public Optional<Movie> getMovieById(UUID movieId) {
+        return movieRepository.findById(movieId);
     }
-    
-    public Movie saveMovie(Movie movie){
-        return movieRepo.save(movie);
+
+    /**
+     * Saves a movie to the database.
+     *
+     * @param movie The movie to be saved.
+     * @return The saved movie entity.
+     */
+    public Movie saveMovie(Movie movie) {
+        return movieRepository.save(movie);
     }
-    
-    
-    public List<Movie> getMoviesFromTMDB() {
-        // the logic to fetch from TMDB API here
-        return null;
+
+    /**
+     * Deletes a movie from the database.
+     *
+     * @param movieId The UUID of the movie to be deleted.
+     */
+    public void deleteMovie(UUID movieId) {
+        movieRepository.deleteById(movieId);
     }
-    
-    
 }

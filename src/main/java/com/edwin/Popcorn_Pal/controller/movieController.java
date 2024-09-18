a146eee782f8c6e8a88ae3b140f9ba3d3b19e781
edwinjoel1202 <edwinjoel1204@gmail.com>
2024-09-18 22:53:06 +0530

    Controller layer package created and MovieController.java added

---
+++ src/main/java/com/edwin/Popcorn_Pal/controller/movieController.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.edwin.Popcorn_Pal.controller;
+
+import com.edwin.Popcorn_Pal.model.Movie;
+import com.edwin.Popcorn_Pal.service.movieService;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author edwin
+ */
+@RestController
+@RequestMapping("/movies")
+public class movieController {
+    
+    @Autowired
+    private movieService movieService;
+    
+    @GetMapping
+    public ResponseEntity<List<Movie>> getAllMovies() {
+        List<Movie> movies = movieService.getAllMovies();
+        return ResponseEntity.ok(movies);
+    }
+    
+//    @GetMapping("/search")
+//    public ResponseEntity<List<Movie>> searchMovies(@RequestParam("title") String title) {
+//        List<Movie> movies = movieService.searchMoviesByTitle(title);
+//        return ResponseEntity.ok(movies);
+//    }
+    
+    @GetMapping("/{movieId}")
+    public ResponseEntity<Movie> getMovieById(@PathVariable UUID movieId) {
+        Optional<Movie> movie = movieService.getMovieById(movieId);
+        return movie.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+}

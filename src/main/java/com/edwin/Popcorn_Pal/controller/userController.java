ad12c0471ae115d6cb4d7d1972d7af08283dbadc
edwinjoel1202 <edwinjoel1204@gmail.com>
2024-09-19 19:33:14 +0530

    UserController layer added

---
+++ src/main/java/com/edwin/Popcorn_Pal/controller/userController.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.edwin.Popcorn_Pal.controller;
+
+import com.edwin.Popcorn_Pal.model.User;
+import com.edwin.Popcorn_Pal.service.userService;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+/**
+ *
+ * @author edwin
+ */
+public class userController {
+    
+    @Autowired
+    private userService userService;
+
+    // Register a new user
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody User user) {
+        Optional<User> existingUser = userService.getUserByUsername(user.getUsername());
+        if (existingUser.isPresent()) {
+            return new ResponseEntity<>("Username already taken", HttpStatus.CONFLICT);
+        }
+        userService.saveUser(user);
+        return new ResponseEntity<>("User registered successfully", HttpStatus.CREATED);
+    }
+
+    // User login
+    @PostMapping("/login")
+    public ResponseEntity<String> loginUser(@RequestBody User loginRequest) {
+        Optional<User> user = userService.authenticateUser(loginRequest.getUsername(), loginRequest.getPassword());
+        if (user.isPresent()) {
+            return new ResponseEntity<>("Login successful", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Invalid username or password", HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+    // Get user by ID
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable UUID id) {
+        Optional<User> user = userService.getUserById(id);
+        if (user.isPresent()) {
+            return new ResponseEntity<>(user.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+    
+    // Get all users (for administrative or development purposes)
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+}

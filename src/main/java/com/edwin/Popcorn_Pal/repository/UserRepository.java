fa166d8058d74bcba756dd695d566039051694bb
edwinjoel1202 <edwinjoel1204@gmail.com>
2024-09-19 19:32:49 +0530

    findByUsername method added in userRepository

--- src/main/java/com/edwin/Popcorn_Pal/repository/UserRepository.java
+++ src/main/java/com/edwin/Popcorn_Pal/repository/UserRepository.java
@@ -6,6 +6,7 @@
 package com.edwin.Popcorn_Pal.repository;
 
 import com.edwin.Popcorn_Pal.model.User;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -19,5 +20,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID>{
-    
+    Optional<User> findByUsername(String username);  // Query to find user by username
 }

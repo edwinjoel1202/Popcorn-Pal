b4b6f591f217b41bdb2d0a04d9b5b072a30ee436
edwinjoel1202 <edwinjoel1204@gmail.com>
2024-09-29 14:10:56 +0530

    movieController.java modified

--- src/main/java/com/edwin/Popcorn_Pal/controller/movieController.java
+++ src/main/java/com/edwin/Popcorn_Pal/controller/movieController.java
@@ -1,12 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.edwin.Popcorn_Pal.controller;
 
 import com.edwin.Popcorn_Pal.model.Movie;
 import com.edwin.Popcorn_Pal.service.movieService;
-import java.util.List;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,10 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- *
- * @author edwin
- */
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/movies")
@@ -29,17 +22,17 @@ public class movieController {
 
     // Get top 20 trending movies
     @GetMapping("/trending")
-    public ResponseEntity<List<Movie>> getTrendingMovies() {
-        List<Movie> trendingMovies = movieService. getTrendingMoviesFromTMDB();
+    public ResponseEntity<JsonNode> getTrendingMovies() {
+        JsonNode trendingMovies = movieService.getTrendingMoviesFromTMDB();
         return new ResponseEntity<>(trendingMovies, HttpStatus.OK);
     }
 
     // Get movie by title
     @GetMapping("/title/{title}")
     public ResponseEntity<Movie> getMovieByTitle(@PathVariable String title) {
-        Movie movie = movieService.getMovieByTitle(title);
-        if (movie != null) {
-            return new ResponseEntity<>(movie, HttpStatus.OK);
+        Optional<Movie> movieOptional = movieService.getMovieByTitle(title);
+        if (movieOptional.isPresent()) {
+            return new ResponseEntity<>(movieOptional.get(), HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }

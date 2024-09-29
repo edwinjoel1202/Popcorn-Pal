dcee36faa836f7d907568aeb391e772d20ed8ce1
edwinjoel1202 <edwinjoel1204@gmail.com>
2024-09-29 14:08:54 +0530

    UUID changed to Long and abstract method added in the interface

--- src/main/java/com/edwin/Popcorn_Pal/repository/MovieRepository.java
+++ src/main/java/com/edwin/Popcorn_Pal/repository/MovieRepository.java
@@ -1,21 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package com.edwin.Popcorn_Pal.repository;
 
 import com.edwin.Popcorn_Pal.model.Movie;
-import java.util.Optional;
-import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
-/**
- *
- * @author edwin
- */
-
-@Repository
-public interface MovieRepository extends JpaRepository<Movie, UUID>{
+public interface MovieRepository extends JpaRepository<Movie, Long> {
     Optional<Movie> findByTitle(String title);
 }
